2546769f34dcf76725f711b51ecaf8430b955ad5
Brady Pierce <bradypierce13@gmail.com>
2023-04-23 14:06:15 -0700

    Add format on save

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -9,14 +9,35 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.deno_fmt,                                                    -- choosed deno for ts/js files cuz its very fast!
   b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
 
   -- Lua
-  b.formatting.stylua
+  b.formatting.stylua,
+
+  -- Elixir
+  b.formatting.mix,
+  b.diagnostics.credo,
+
+  -- Rust
+  b.formatting.rustfmt,
 }
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
+      })
+    end
+  end,
 }
