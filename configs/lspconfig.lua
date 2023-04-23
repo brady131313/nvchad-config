fd5b34e07baca3a9ba4050323f6a9596c5976a13
Brady Pierce <bradypierce13@gmail.com>
2023-04-23 13:43:48 -0700

    Initial commit

---
+++ configs/lspconfig.lua
@@ -0,0 +1,25 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "rust_analyzer", "tailwindcss" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- 
+-- lspconfig.pyright.setup { blabla}
+--
+
+local elixir_cmd = vim.fn.stdpath('data') .. "/mason/bin/elixir-ls"
+lspconfig.elixirls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { elixir_cmd }
+}
